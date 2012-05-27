c65bb495904161c21b5e7b3ca47f12880fdc6621
Aaron Gundel <aaron.gundel@gmail.com>
2012-05-26 22:07:36 -0400

    Added mongo support, crstarted create flock functionality

--- app.js
+++ app.js
@@ -30,6 +30,8 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
+app.get('/flock/', routes.flock.index);
+app.get('/flock/create', routes.flock.create);
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,15 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: 'Express' });
+};
+var flocks = [{name: "Aaron's flock"}, {name: "Kron's flock"}]
+exports.flock = {};
+exports.flock.index = function(req, res){
+  res.render('flock/index', { title: 'Flocks', flocks: flocks });
+};
+exports.flock.create = function(req, res){
+  req.customCss = ["/stylesheets/leaflet.css"];
+  req.customIeCss = ["/stylesheets/leaflet.ie.css"];
+  res.render('flock/create', { title: 'Create a new Flock', flocks: flocks, req: req });
 };
