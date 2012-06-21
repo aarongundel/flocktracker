d061ece32f26801d1e95c2f3a907d4145e65b93a
Aaron Gundel <aaron.gundel@gmail.com>
2012-06-21 16:49:56 -0700

    Trying to figure out schema

--- routes/index.js
+++ routes/index.js
@@ -2,30 +2,38 @@
 /*
  * GET home page.
  */
-var mongoose = require('mongoose'), 
-  Schema, ObjectId, Flock;
+var mongoose = require('mongoose'), Schema, ObjectId, Flock;
 
-mongoose.connect('mongodb://localhost/flock_tracker', function(err) { 
-    if (!err) { 
-      console.log('connected to MongoDB'); 
-    } else { 
-      throw err; 
-    } 
-}); 
+mongoose.connect('mongodb://localhost/flock_tracker', function (err) {
+  if (err) {
+    return 0; //TODO: Do something meaningful if we couldn't connect to mongo
+  }
+});
 
 Schema = mongoose.Schema;
 ObjectId = Schema.ObjectId;
 
-exports.index = function(req, res){
+Flock = new Schema({
+  organizer: String,
+  name: String
+});
+
+exports.index = function (req, res) {
   res.render('index', { title: 'Express' });
 };
-var flocks = [{name: "Aaron's flock"}, {name: "Kron's flock"}]
+//var flocks = [{name: "Aaron's flock"}, {name: "Kron's flock"}]
 exports.flock = {};
-exports.flock.index = function(req, res){
-  res.render('flock/index', { title: 'Flocks', flocks: flocks });
+exports.flock.index = function (req, res) {
+  Flock.find({}, function (err, docs) {
+    res.render('flock/index', {title: 'Flocks', flocks: docs});
+  });
 };
-exports.flock.create = function(req, res){
+exports.flock.create = function (req, res) {
   req.customCss = ["/stylesheets/leaflet.css"];
   req.customIeCss = ["/stylesheets/leaflet.ie.css"];
-  res.render('flock/create', { title: 'Create a new Flock', flocks: flocks, req: req });
+  res.render('flock/create', {
+    title: 'Create a new Flock',
+    flocks: flocks,
+    req: req 
+  });
 };
